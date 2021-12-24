5f93ad01e62ed9c8886d7e6d46f71fa941c464cb
fadihere <fadihere@outlook.com>
2021-12-24 12:13:49 +0500

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,75 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Color.fromARGB(255, 49, 49, 49),
         body: SafeArea(
-          child: Column(children: <Widget>[
-            CircleAvatar(
-              radius: 70.0,
-              backgroundImage: AssetImage('images/fs.jpg'),
-            ),
-            Text(
-              'Fahad Saleem',
-              style: TextStyle(
-                fontFamily: 'Pushster-Regular',
-                fontSize: 40.0,
-                color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: AssetImage('images/fs.jpg'),
               ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white,
-                fontWeight: FontWeight.normal,
-                letterSpacing: 1.0,
+              Text(
+                'Fahad Saleem',
+                style: TextStyle(
+                  fontFamily: 'Pushster-Regular',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
               ),
-            ),
-          ]),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: 1.0,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+                width: 150,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.call_rounded,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    '+92 321 6959586',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email_rounded,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'fadihere@outlook.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
